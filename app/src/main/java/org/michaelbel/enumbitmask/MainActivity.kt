d1520d54f656774f01341f375f7afa959c0b44e8
michaelbel <michael-bel@outlook.com>
2022-06-04 20:25:34 +0300

    Update project

--- app/src/main/java/org/michaelbel/enumbitmask/MainActivity.kt
+++ app/src/main/java/org/michaelbel/enumbitmask/MainActivity.kt
@@ -3,11 +3,13 @@ package org.michaelbel.enumbitmask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlin.properties.Delegates
+import kotlin.properties.Delegates.notNull
 import org.michaelbel.enumbitmask.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding by notNull()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
