b886b7c8e59ee18d41691d4e860598a2739b7521
michaelbel <michael-bel@outlook.com>
2025-01-28 12:00:25 +0300

    Update project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "org.michaelbel.enumbitmask"
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
-        versionName = "1.0.0"
         versionCode = gitCommitsCount
+        versionName = "1.0.0"
     }
 
     buildFeatures {
